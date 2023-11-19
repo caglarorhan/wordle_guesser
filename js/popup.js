bb6aeef1439694d75f13a781ce1d922ae131b1c5
caglarorhan <caglaror@gmail.com>
2023-11-19 01:54:15 -0600

    small manifest updates

--- js/popup.js
+++ js/popup.js
@@ -5569,8 +5569,9 @@ const WG ={
             },
     },
     sendMessageToContent:function (messageObject){
+
         chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, messageObject)
+            chrome.tabs.sendMessage(tabs[0].id, messageObject);
         });
     },
     setLabel:()=>{
