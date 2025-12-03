d3a2b3a861c469dbb68bef1e665144bfea27bf75
Caglar Orhan <caglaror@gmail.com>
2025-12-03 02:14:38 -0600

    debug: contentjs communication error ahdled

--- js/popup.js
+++ js/popup.js
@@ -5570,7 +5570,11 @@ const WG ={
     },
     sendMessageToContent:function (messageObject){
         chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, messageObject);
+            if (tabs[0]?.id) {
+                chrome.tabs.sendMessage(tabs[0].id, messageObject).catch(() => {
+                    // Content script not available on this page
+                });
+            }
         });
     },
     setLabel:()=>{
